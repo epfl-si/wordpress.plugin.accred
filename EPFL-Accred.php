fed94af4808513f6e2a52129e877c2cf9adf70a5
LuluTchab <l.chaboudez@gmail.com>
2018-10-30 08:08:51 +0100

    Update version to differentiate 'vpsi' version

--- EPFL-Accred.php
+++ EPFL-Accred.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: EPFL Accred
  * Description: Automatically sync access rights to WordPress from EPFL's institutional data repositories
- * Version:     0.11
+ * Version:     0.11 (vpsi)
  * Author:      Dominique Quatravaux
  * Author URI:  mailto:dominique.quatravaux@epfl.ch
  */
